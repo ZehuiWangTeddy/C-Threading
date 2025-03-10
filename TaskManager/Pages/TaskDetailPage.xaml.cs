c11c4fdc6956dcb530864696d10eef46e60c58fc
ZehuiWangTeddy <111961563+ZehuiWangTeddy@users.noreply.github.com>
2025-03-11 00:40:23 +0100

    add task detail page

---
+++ TaskManager/Pages/TaskDetailPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace TaskManager.Pages
+{
+    public partial class TaskDetailPage : ContentPage
+    {
+        public TaskDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
