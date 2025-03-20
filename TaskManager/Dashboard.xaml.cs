a5c90486732a644d110606cfdc62c57c8db5f8b5
iamjames5 <lennox.james@student.nhlstenden.com>
2025-03-20 14:13:32 +0100

    Added graphs and dashboard page

---
+++ TaskManager/Dashboard.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TaskManager
+{
+    public partial class Dashboard : ContentPage
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnViewGraphsClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new Graph());
+        }
+    }
+}
---
+++ TaskManager/Graph.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microcharts;
+using SkiaSharp;
+using Microsoft.Maui.Controls;
+using Microcharts.Maui; // Ensure this is included
+
+namespace TaskManager
+{
+    public partial class Graph : ContentPage
+    {
+         ChartEntry[] entries = new[]
+        {
+            new ChartEntry(212)
+            {
+                Label = "Windows",
+                ValueLabel = "112",
+                Color = SKColor.Parse("#2c3e50")
+            },
+            new ChartEntry(248)
+            {
+                Label = "Android",
+                ValueLabel = "648",
+                Color = SKColor.Parse("#77d065")
+            },
+            new ChartEntry(128)
+            {
+                Label = "iOS",
+                ValueLabel = "428",
+                Color = SKColor.Parse("#b455b6")
+            },
+            new ChartEntry(514)
+            {
+                Label = ".NET MAUI",
+                ValueLabel = "214",
+                Color = SKColor.Parse("#3498db")
+            }
+        };
+
+        public Graph ()
+        {
+            InitializeComponent();
+
+            chartView.Chart = new BarChart
+            {
+                Entries = entries
+            };
+
+            chartView1.Chart = new LineChart
+            {
+                Entries = entries
+            };
+        }
+    }
+}
